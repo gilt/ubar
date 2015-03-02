9ba6a8c5b43e01fddf5c518afa27ff35f6b4c48d
Gregory Mazurek <gmazurek@gilt.com>
2015-03-02 17:16:22 -0500

    some comments on config.js to explain what these variables mean

--- js/ubar/config.js
+++ js/ubar/config.js
@@ -16,12 +16,40 @@
 
     'use strict';
 
+    /**
+    * Expiration Time:
+    *
+    * Disabled sets the amount of time the banner will
+    * not appear to the user. It's essentially the same
+    * as if the user said "ignore."
+    *
+    * Enabled sets the amount of time the redirection
+    * will take place. This exists once a user elects
+    * to participate in ubar.
+    *
+    * Redirected sets the amount of time the user has
+    * on the web before being redirected again. Imagine
+    * the user is redirected but wants to manager her
+    * settings. She has this amount of time do so before
+    * being redirected again. 
+    *
+    */
     var expiration_time = {
         disabled: moment.duration(2, 'weeks'),
         enabled: moment.duration(1, 'year'),
         redirected: moment.duration(60, 'seconds')
       },
 
+    /**
+    * Redirect Interval:
+    *
+    * ios_app_store is the amount of time we suspect it
+    * should take for a user to have been redirected to
+    * the iOS App Store and the minimum amount of time
+    * before which a user could possibly return to the
+    * website. For example,
+    *
+    */
       redirect_interval = {
         ios_app_store: moment.duration(2, 'seconds')
       };
