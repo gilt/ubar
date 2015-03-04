c3ca10e11f7a42d5a104cdca46b20389ecb7e949
Gregory Mazurek <gregory.mazurek@gmail.com>
2015-03-04 11:48:26 -0500

    first pass at breaking defaultConfig into sections
    first pass at breaking defaultConfig into sections

--- js/ubar/config.js
+++ js/ubar/config.js
@@ -17,19 +17,50 @@
     'use strict';
 
     var defaultConfig = {
+      
+      urls : {
         ios_app_store_url       : 'https://itunes.apple.com/us/app/appname/id331804452?mt=8',
         app_deep_link           : 'gilt://',
+      },
+      
+      templates : {
         sending_template_path   : '../templates/ubar/ubar_sending',
         returning_template_path : '../templates/ubar/ubar_returning',
-        enabled_time            : '1 year',
-        disabled_time           : '2 weeks',
-        manage_window_time      : '60 seconds',
+      },
+      
+      
+      /**
+      * Timing:
+      *
+      * Disabled sets the amount of time the banner will
+      * not appear to the user. It's essentially the same
+      * as if the user said "ignore."
+      *
+      * Enabled sets the amount of time the redirection
+      * will take place. This exists once a user elects
+      * to participate in ubar.
+      *
+      * Redirected sets the amount of time the user has
+      * on the web before being redirected again. Imagine
+      * the user is redirected but wants to manager her
+      * settings. She has this amount of time do so before
+      * being redirected again. 
+      *
+      */
+      timing : {
+        enabled            : '1 year',
+        disabled           : '2 weeks',
+        manage_window      : '60 seconds',
+      },
+      
+      classNames : {
         component_class         : 'component-ubar',
         on_class                : 'ubar-on-button',
         install_class           : 'ubar-install-app-button',
         off_class               : 'ubar-off-button',
         open_in_app_class       : 'ubar-open-in-app-button',
         close_class             : 'ubar-close-banner-button'
+      }
     },
 
     /**
