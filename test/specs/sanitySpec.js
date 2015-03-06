0075c14b4364d01241bb8c141c0039de9e3812f2
Kyle Dorman <kdorman@gilt.com>
2015-03-06 16:45:56 -0500

    remerging greg's commit. #gitproblems

--- test/specs/sanitySpec.js
+++ test/specs/sanitySpec.js
@@ -1,16 +1,8 @@
-var requirejs = require('requirejs');
-var chai = require('chai');
-var should = chai.should();
-
-describe('basic sanity test', function () {
+define(function(require) {
+ 
+  describe('Sanity', function() {
   it('had better work', function () {
     true.should.equal(true);
   });
-
-  it('be false. Fail damn it', function () {
-    requirejs(['ubar_storage'], function(storage) {
-      (false.should.equal(true)).should.equal(false);
-      false.should.not.equal(true);
-    });
   });
 });
---
+++ test/specs/trackingSpec.js
@@ -0,0 +1,10 @@
+define(function(require) {
+
+	var ubar_tracking = require('ubar_tracking');
+
+	describe('Tracking', function() {
+	  it('should call _turnUbarOn', function() {
+			console.log(ubar_tracking);
+		});
+	});
+});
