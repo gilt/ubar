87b0bf2742f40787c30dac71856b13d3eff9e876
Archana Kumar <akumar@gilt.com>
2015-03-26 12:10:03 -0400

    updated index page layout and styling

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,6 +4,7 @@ var debug = require('gulp-debug');
 var BASE_FILE = require('../config').js.BASE_FILE;
 var JS_SOURCE_FILES = require('../config').js.SOURCE_FILES;
 var CSS_SOURCE_FILES = require('../config').css.SOURCE_FILES;
+var CSS_PAGE_BASE_FILE = require('../config').css.PAGE_FILE;
 var SPEC_SOURCE_FILES = require('../config').spec.SOURCE_FILES;
 
 gulp.task('watch', function() {
@@ -11,6 +12,7 @@ gulp.task('watch', function() {
   gulp.watch(BASE_FILE, ['js', 'mochify-phantomjs']);
   gulp.watch(JS_SOURCE_FILES, ['js' /*, 'test' */ ]);
   gulp.watch(CSS_SOURCE_FILES, ['less']);
+  gulp.watch(CSS_PAGE_BASE_FILE, ['less-page']);
   gulp.watch(SPEC_SOURCE_FILES, ['mochify']);
 
 });
