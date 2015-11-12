b4fb80fb35ae88cbeff844f151ead7aa1f8b1d9a
Kyle Dorman <kdorman@gilt.com>
2015-11-12 17:14:12 -0500

    Updating dev dependencies

--- gulp/tasks/run.js
+++ gulp/tasks/run.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
 var beefy = require('beefy');
 var http = require('http');
-var handler = beefy(require('../config').js.JS_DEST_BROWSERIFIED_FILE);
+var file = require('../config').js.JS_DEST_BROWSERIFIED_FILE;
+var handler = beefy({
+	entries: [file],
+	bundler: "./node_modules/browserify"
+});
 
 gulp.task('run', function(){
   return http.createServer(handler).listen(8000);
